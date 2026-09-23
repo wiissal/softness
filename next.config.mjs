3e9270c6c1ec0ebcfcd942c65a87112377cefd00
wiissal <wissaloa1@gmail.com>
2026-09-23 16:33:50 +0000

    chore: remove Unsplash remote image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
-  },
 };
 
 export default nextConfig;
